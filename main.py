919bd2118b6d0603f57d86c816668c1fcae2fbb4
Amandeep <ads71993@gmail.com>
2017-12-12 18:36:19 -0500

    updated main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from experiments.backpropogation.run import run as bp_run
 from experiments.knn.run import run as knn_run
 from experiments.logistic_regresssion.run import run as lr_run
 
-knn_run()
-lr_run()
+# knn_run()
+# lr_run()
+bp_run()
