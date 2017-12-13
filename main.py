856603bf212ce1e0e8f4bf00cd61b16dec749ae7
Amandeep <ads71993@gmail.com>
2017-12-13 01:38:52 -0500

    update

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from experiments.knn.k_variation import k_variation
 from experiments.knn.run import run as knn_run
 from experiments.logistic_regresssion.run import run as lr_run
 
-# knn_run()
-# lr_run()
-k_variation()
-
+knn_run()
+lr_run()
 bp_run()
